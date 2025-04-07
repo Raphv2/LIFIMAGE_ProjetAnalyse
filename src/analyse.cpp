9fa803bb0e81acb598da24dbfa502aef7f903238
zbeubizbeub <raphael.gosset02000@gmail.com>
2025-04-07 10:53:43 +0200

    add example for "synthese"

--- src/analyse.cpp
+++ src/analyse.cpp
@@ -2,110 +2,124 @@
  * @file  analyse.cpp
  * @brief Contains the main program of the project
  **************************************************************************************************/
-#include <opencv4/opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
+ #include <opencv4/opencv2/opencv.hpp>
+ #include <iostream>
+ #include <vector>
 #include <cstdlib> 
 #include <ctime>   
  
-cv::Scalar getDominantColor(cv::Mat& imageHSV) {
-    std::vector<cv::Mat> hsvChannels;
-    cv::split(imageHSV, hsvChannels);
+ cv::Scalar getDominantColor(cv::Mat& imageHSV) {
+     std::vector<cv::Mat> hsvChannels;
+     cv::split(imageHSV, hsvChannels);
+ 
+     int histSize = 180; 
+     float range[] = {0, 180}; 
+     const float* histRange = {range};
+     cv::Mat hist;
+ 
+     cv::calcHist(&hsvChannels[0], 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, true, false);
+ 
+     double maxVal = 0;
+     cv::Point maxLoc; 
+     cv::minMaxLoc(hist, 0, &maxVal, 0, &maxLoc);
+     
+     int dominantHue = maxLoc.y; 
+ 
+     double avgSaturation = cv::mean(hsvChannels[1])[0];
+     double avgValue = cv::mean(hsvChannels[2])[0];
+ 
+     return cv::Scalar(dominantHue, avgSaturation, avgValue);  
+ }
 
-    int histSize = 180; 
-    float range[] = {0, 180}; 
-    const float* histRange = {range};
-    cv::Mat hist;
+ cv::Scalar getRandomColor() {
+    return cv::Scalar(std::rand() % 256, std::rand() % 256, std::rand() % 256);
+}
+ 
+ int main() {
+     cv::Mat inputImage = cv::imread("data/fleur.jpg");
+     if (inputImage.empty()) {
+         std::cerr << "Erreur lors du chargement de l'image." << std::endl;
+         return -1;
+     }
 
-    cv::calcHist(&hsvChannels[0], 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, true, false);
+     int width = inputImage.cols;
+     int height = inputImage.rows;
+ 
+     cv::Rect resize(10 , 10 , width * 0.9, height - 20); 
+ 
+     cv::Mat croppedImage = inputImage(resize);
+ 
+     cv::Mat imageHSV;
+     cv::cvtColor(croppedImage, imageHSV, cv::COLOR_BGR2HSV);
+ 
+     cv::Scalar dominantColor = getDominantColor(imageHSV);
+     std::cout << "Teinte dominante : " << dominantColor[0] << std::endl;
+ 
+     int hueRange = 5; 
+     //int hueRange = 10; 
+     //cv::Scalar lowerBound(dominantColor[0] - hueRange, 170, 170);
+     //cv::Scalar upperBound(dominantColor[0] + hueRange, 255, 255);
+        cv::Scalar lowerBound(dominantColor[0] - hueRange, dominantColor[1], dominantColor[2]);
+        cv::Scalar upperBound(dominantColor[0] + hueRange, 255, 255);
+ 
+     cv::Mat mask;
+     cv::inRange(imageHSV, lowerBound, upperBound, mask);
+     cv::bitwise_not(mask, mask);
 
-    double maxVal = 0;
-    cv::Point maxLoc; 
-    cv::minMaxLoc(hist, 0, &maxVal, 0, &maxLoc);
+     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+     cv::erode(mask, mask, kernel);
+ 
+
+     cv::dilate(mask, mask, kernel);
+
+     std::vector<std::vector<cv::Point>> contours;
+     std::vector<cv::Vec4i> hierarchy;
+     cv::findContours(mask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+     
+     cv::Mat contourImage = cv::Mat(height, width, CV_8UC3, cv::Scalar(255, 255, 255));
     
-    int dominantHue = maxLoc.y; 
+     for (size_t i = 0; i < contours.size(); ++i) {
 
-    return cv::Scalar(dominantHue, 100, 100); 
-}
+        cv::Mat pieceMask = cv::Mat::zeros(croppedImage.size(), CV_8UC1);
+        cv::drawContours(pieceMask, contours, static_cast<int>(i), cv::Scalar(255), cv::FILLED);
 
-cv::Scalar getRandomColor() {
-return cv::Scalar(std::rand() % 256, std::rand() % 256, std::rand() % 256);
-}
+        cv::Rect boundingBox = cv::boundingRect(contours[i]);
 
-int main() {
-    cv::Mat inputImage = cv::imread("data/puzzle.jpg");
-    if (inputImage.empty()) {
-        std::cerr << "Erreur lors du chargement de l'image." << std::endl;
-        return -1;
+        cv::Mat piece;
+        croppedImage.copyTo(piece, pieceMask); 
+
+        cv::Mat croppedPiece = piece(boundingBox);
+        cv::Mat croppedMask = pieceMask(boundingBox);
+
+        croppedPiece.copyTo(contourImage(boundingBox), croppedMask);
     }
 
-    int width = inputImage.cols;
-    int height = inputImage.rows;
+    int objIndex = 0;
+    for (const auto& contour : contours) {
 
-    cv::Rect resize(10 , 10 , width * 0.9, height - 20); 
+        cv::Rect boundingBox = cv::boundingRect(contour);
 
-    cv::Mat croppedImage = inputImage(resize);
+        cv::Mat roi = contourImage(boundingBox);
 
-    cv::Mat imageHSV;
-    cv::cvtColor(croppedImage, imageHSV, cv::COLOR_BGR2HSV);
-
-    cv::Scalar dominantColor = getDominantColor(imageHSV);
-    std::cout << "Teinte dominante : " << dominantColor[0] << std::endl;
-
-    int hueRange = 5; 
-    cv::Scalar lowerBound(dominantColor[0] - hueRange, 170, 170);
-    cv::Scalar upperBound(dominantColor[0] + hueRange, 255, 255);
-
-    cv::Mat mask;
-    cv::inRange(imageHSV, lowerBound, upperBound, mask);
-    cv::bitwise_not(mask, mask);
-
-    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-    cv::erode(mask, mask, kernel);
+        std::string filename = "data/object_" + std::to_string(objIndex) + ".png";
+        cv::imwrite(filename, roi);
+        objIndex++;
+    }
 
 
-    cv::dilate(mask, mask, kernel);
-
-    std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(mask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    
-    cv::Mat contourImage = cv::Mat(height, width, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    for (size_t i = 0; i < contours.size(); ++i) {
-
-    cv::Mat pieceMask = cv::Mat::zeros(croppedImage.size(), CV_8UC1);
-    cv::drawContours(pieceMask, contours, static_cast<int>(i), cv::Scalar(255), cv::FILLED);
-
-    cv::Rect boundingBox = cv::boundingRect(contours[i]);
-
-    cv::Mat piece;
-    croppedImage.copyTo(piece, pieceMask); 
-
-    cv::Mat croppedPiece = piece(boundingBox);
-    cv::Mat croppedMask = pieceMask(boundingBox);
-
-    croppedPiece.copyTo(contourImage(boundingBox), croppedMask);
-}
-
-int objIndex = 0;
-for (const auto& contour : contours) {
-
-    cv::Rect boundingBox = cv::boundingRect(contour);
-
-    cv::Mat roi = contourImage(boundingBox);
-
-    std::string filename = "data/object_" + std::to_string(objIndex) + ".png";
-    cv::imwrite(filename, roi);
-    objIndex++;
-}
-
-    cv::imshow("Image Originale (Recadrée)", croppedImage);
-    cv::imshow("Masque basé sur la couleur dominante", mask);
-    cv::imshow("Contours trouvés", contourImage);
-
-    cv::waitKey(0);
 
 
-    return 0;
-}
+     // Affichage
+     cv::imshow("Image Originale (Recadrée)", croppedImage);
+     cv::imshow("Masque basé sur la couleur dominante", mask);
+     cv::imshow("Contours trouvés", contourImage);
+
+ 
+     cv::waitKey(0);
+
+ 
+     return 0;
+ }
+ 
+
+ 
--- src/synthese.cpp
+++ src/synthese.cpp
@@ -5,7 +5,84 @@
 
 #include <iostream>
 #include <stdexcept>
+#include "vec.h"
+#include <cmath>
+#include "color.h"
+#include "image.h"
+#include "image_io.h"
 
-int main() {
+
+float intersect_sphere( /* parametres de la sphere */ const Point& c, const float r, /* parametres du rayon */ const Point& o, const Vector& d )
+{
+
+    Vector co = Vector(c, o);
+    float a= dot(d,d);
+    float b= 2 * dot(d, co);
+    float k= dot(co, co) - (r*r) ;
+
+    float dd = b *b - 4* a*k;
+
+    if ( dd < 0)
+    return -1;
+
+    float t1 = (-b + sqrt(dd)) / (2 * a);
+    float t2 = (-b - sqrt(dd)) / (2 * a);
+    float t = std::min(t1, t2);
+    if (t < 0) return -1;
+    else return t;
+}
+
+float intersect_plan( /* parametres du plan */ const Point& a, const Vector& n, /* parametres du rayon */ const Point& o, const Vector& d )
+{
+    // intersection avec le rayon o , d
+    float t= dot (n , Vector (o , a) ) / dot (n , d);
+
+    return t;
+
+}
+
+
+
+
+int main( )
+{
+    // cree l'image resultat
+    Image image(1024, 512);    // par exemple...
+    Point c = Point(0, 0, -3);
+    float r = 2;
+    Point a = Point(0,-1,0);
+    Vector n = Vector(0,1,0);
+    Vector l= Vector(-4, 6, 1);
+    Color emission = Color(200,0,200);
+
+    for(int py= 0; py < image.height(); py++)
+    for(int px= 0; px < image.width(); px++)
+    {        
+        Point o= Point(0, 0, 0);    // origine
+        Point e = Point(px - image.width()/2, py - image.height()/2, -100);
+        Vector d= normalize(Vector(o, e));     // direction : extremite - origine
+        float i = intersect_sphere( /* parametres de la sphere */ c, r, /* parametres du rayon */ o, d );
+        float j = intersect_plan( /* parametres du plan */ a, n, /* parametres du rayon */ o, d );
+        Point p= o + i *d;
+        Vector n_sphere = normalize(p - c);
+        float cos_theta = 0;
+        
+        if(i >= 0.0f && j < 0){
+            cos_theta = std::max(dot(n_sphere, normalize(l)), 0.0f);
+            image(px, py)= Red() * emission * cos_theta ;
+        }
+        else if(j >= 0.0f && i < 0){
+            cos_theta = std::max(dot(normalize(n), normalize(l)), 0.0f);
+            image(px, py) = Blue() * emission * cos_theta ;
+        }
+        else if(j >= 0.0f && i >= 0.0f){
+            cos_theta = std::max(i < j ? dot(n_sphere, normalize(l)) : dot(normalize(n), normalize(l)), 0.0f);
+            image(px, py) = i < j ? Red()  * emission * cos_theta : Blue() * emission * cos_theta  ;
+        }
+
+        
+    }
+
+    write_image(image, "image.png");
     return 0;
 }
